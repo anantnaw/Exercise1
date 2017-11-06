4153c2deb421b63e82e66493164fe324b2e17be5
Unknown <anant.na@gmail.com>
2017-11-06 10:17:53 +0100

    bugfix

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,10 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("Hello ASE2017 - How are you now? :)");
-	}
-=======
+	
 		System.out.println("Hello ASE2017 - How are you? :)");
 		System.out.print("Please enter your name");
 		Scanner in= new Scanner(System.in);
@@ -15,6 +14,6 @@ public class HelloWorld {
 		HelloUser hu = new HelloUser(userName);
 		hu.greetUser();
 		}
->>>>>>> addhellobranch
+
 
 }
